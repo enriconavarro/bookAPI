6b9127a37ef6b1be64c2ad058883dc79162581c3
Enrico Navarro <enricolimanavarro@gmail.com>
2019-02-18 23:13:41 -0300

    Adicionando comentários na migration 00.

--- backend/src/main/resources/db/__00__.sql
+++ backend/src/main/resources/db/__00__.sql
@@ -1,10 +1,14 @@
+--- Execute primeiro essa linha
+
 CREATE DATABASE book_api ENCODING='UTF8' LC_COLLATE='pt_BR.UTF-8' LC_CTYPE='pt_BR.UTF-8' TEMPLATE=template0;
+
+--- Execute depois esse bloco
 
 CREATE ROLE book_api LOGIN
   ENCRYPTED PASSWORD 'book_api'
   SUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
 
---- Dentro do database criado acima
+--- Execute dentro do database criado acima
 
 CREATE SCHEMA book_api;
 
